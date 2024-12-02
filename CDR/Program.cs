43c1b11701abbc4ef71be2b1309b2759f88ec62a
Maren Gjermundrød <46926679+marenandrea@users.noreply.github.com>
2024-12-02 10:40:07 +0100

    Added error handling and changed type of StartTime to DateTime.

--- CDR/Program.cs
+++ CDR/Program.cs
@@ -1,27 +1,37 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 
-var data = File.ReadAllText("cdrs.json");
-var cdrs = JsonSerializer.Deserialize<List<Cdr>>(data);
-
-Console.WriteLine("Top 3 Most Active Callers:");
-var topCallers = cdrs.GroupBy(g => g.Caller).OrderByDescending(o => o.Count()).Take(3);
-foreach (var i in topCallers)
+try
 {
-    Console.WriteLine($"{i.Key}: {i.Count()} calls");
+    var data = File.ReadAllText("cdrs.json");
+    var cdrs = JsonSerializer.Deserialize<List<Cdr>>(data) ?? throw new Exception("Deserialization failed.");
+    if (cdrs.Count == 0)
+    {
+        throw new Exception("Call Detail Records are missing.");
+    }
+
+    var topThreeCallers = cdrs.GroupBy(g => g.Caller).OrderByDescending(o => o.Count()).Take(3);
+    Console.WriteLine("Top 3 Most Active Callers:");
+    foreach (var caller in topThreeCallers)
+    {
+        Console.WriteLine($"{caller.Key}: {caller.Count()} calls");
+    }
+
+    var topCaller = topThreeCallers.First().Key;
+    var durationOfCallsToTopCaller = cdrs.Where(w => w.Receiver == topCaller).Sum(s => s.Duration);
+    Console.WriteLine($"Total Duration of Calls to {topCaller}: {durationOfCallsToTopCaller} seconds");
+
+    var uniquePhoneNumbers = cdrs.SelectMany(s => new List<string> { s.Caller, s.Receiver }).Distinct().Count();
+    Console.WriteLine($"Total Unique Phone Numbers: {uniquePhoneNumbers}");
 }
-
-var topCaller = topCallers.First().Key;
-var durationOfCallsToTopCaller = cdrs.Where(w => w.Receiver == topCaller).Sum(s => s.Duration);
-Console.WriteLine($"Total Duration of Calls to {topCaller}: {durationOfCallsToTopCaller} seconds");
-
-var uniquePhoneNumbers = cdrs.SelectMany(s => new List<string> { s.Caller, s.Receiver }).Distinct().Count();
-Console.WriteLine($"Total Unique Phone Numbers: {uniquePhoneNumbers}");
+catch (Exception e)
+{
+    Console.WriteLine($"Something went wrong: {e.Message}");
+}
 
 public class Cdr
 {
     public string Caller { get; set; }
     public string Receiver { get; set; }
-    public string StartTime { get; set; }
+    public DateTime StartTime { get; set; }
     public int Duration { get; set; }
-
 }
